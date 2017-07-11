651bd33932c478b73388b98afe85621269c5fc7e
wk645 <wk645@nyu.edu>
2017-07-11 09:18:12 -0400

    final

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,6 @@ require 'pry'
 
 class Game
 
-# WORDS = ["abstract", "water", "turing", "glasses", "ribald", "bagpipes", "fervid", "gazebo", "klutz", "ostracize", "phlegm", "yacht", "squawk", "quip", "matrix", "astoria", "brooklyn"]
-
 	def initialize
 		@turns = 7
 		@correct_guess = []
@@ -50,32 +48,24 @@ class Game
 	end
 
 	def pick_a_letter
-		puts "Pick a letter from A to Z, or guess the entire word."
-		@guess = gets.chomp
+		puts "\nPick a letter from A to Z, or guess the whole word."
+		@guess = gets.downcase.chomp
+		if @correct_guess.include?(@guess)
+			puts "You've already correctly guessed that letter, please try something else"
+			pick_a_letter
+		elsif (@guess =~ /[[:alpha:]]/) != 0
+			puts "Letters only, please"
+			pick_a_letter
+		end
 	end
 
-	def guess_correctly?
-		@word.include?(@guess)
-	end
-
-	def correct_guess?
-		if @guess.split(//).sort == @word.sort
-			puts "You win! The word is '#{@word.join}'."
-			@wins +=1
-			@correct_guess.clear
-			play?
-
-		elsif guess_correctly?
-			indexes = @word.each_index.select { |i| @word[i] == @guess}
-			indexes = indexes.map {|i| i + 1}
-			indexes.size.times do
-				@correct_guess << @guess
+	def print_word
+		@word.each_with_index do |l, i|
+			if @correct_guess.include?(l)
+				print "#{l}  "
+			else
+				print "  "
 			end
-			puts "You guessed correctly. The letter you guessed is in slot number(s) #{indexes.join(', ')} in the word."
-		else
-			@turns -= 1
-			puts "You did not guess correctly. You have #{@turns} turn(s) remaining."
-			display_hangman
 		end
 	end
 
@@ -83,27 +73,64 @@ class Game
 		(0..(7 - @turns)).to_a.each{|line_number| puts @hangman_array[line_number]}
 	end
 
-	def won_or_lost?
-		if @word.sort.uniq == @correct_guess.sort.uniq
-			puts "You win! The word is '#{@word.join}'."
-			@wins +=1
-			@correct_guess.clear
-			play?
-		elsif
-			@turns == 0
-			puts "You're out of turns. You've been hanged. Would you like to try again with a new life?"
-			@losses +=1
-			play?
-		end
+	def guess_correctly?
+		@word.include?(@guess) || @word == @guess.split(//)
 	end
 
-	def full_game
-		greeting
-		play?
-		until won_or_lost? 
-			pick_a_letter
-			correct_guess?
-			won_or_lost?
-		end
-	end
+	def whole_word_correct?
+        if @guess.split(//).sort == @word.sort
+            puts "You win! The word is '#{@word.join}'."
+            @wins +=1
+            @correct_guess.clear
+            play?
+        end
+    end
+
+	def correct_guess?
+        if guess_correctly?
+            indexes = @word.each_index.select { |i| @word[i] == @guess}
+            indexes = indexes.map {|i| i + 1}
+            indexes.size.times do
+                @correct_guess << @guess
+            end
+            puts "You guessed correctly. The letter you guessed is in slot number(s) #{indexes.join(', ')} in the word."
+        else
+            @turns -= 1
+            puts "You did not guess correctly. You have #{@turns} turn(s) remaining."
+            display_hangman
+        end
+    end
+
+    def won?
+        if @word.sort.uniq == @correct_guess.sort.uniq
+            puts "You win! The word is '#{@word.join}'."
+            @wins +=1
+            @correct_guess.clear
+            play?
+        end
+    end
+
+    def lost?
+        if @turns == 0
+            puts "\nThe word was '#{@word.join}'. You're out of turns. You've been hanged. Would you like to try again with a new life?"
+            @losses +=1
+            @correct_guess.clear
+            play?
+        end
+    end
+
+    def full_game
+        greeting
+        play?
+        until won? || lost? 
+            pick_a_letter
+            whole_word_correct?
+            correct_guess?
+            print_word
+            won?
+            lost?
+        end
+    end
 end
+
+	
